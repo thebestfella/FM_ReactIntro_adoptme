12e4348f135df7a9150724b768e1f4f369b95381
thebestfella <thisispeter@msn.com>
2020-04-04 18:49:39 -0700

    after Hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./searchParams";
 
 //like a stamp, has to be used in order to create effect
 const App = () => {
-  // comment out the original js, using jxs now
-  // return React.createElement(
-  //   //called 1st here
-  //   "div",
-  //   { id: "some id" }, //attribute you want to add
-  //   [
-  //     React.createElement("h1", {}, "adopt me!"), //called 2nd here
-  //     React.createElement(Pet, {
-  //       name: "luna",
-  //       animal: "dog",
-  //       breed: "havanese",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "pepper",
-  //       animal: "bird",
-  //       breed: "cockatiel",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "doink",
-  //       animal: "cat",
-  //       breed: "mixed",
-  //     }),
-  //   ]
-  // );
-
   return (
     <div>
       <h1 id="something important">Adopt me!</h1>
-      <Pet name="luna" animal="dog" breed="havanese" />
-      <Pet name="pepper" animal="bird" breed="cockatiel" />
-      <Pet name="doink" animal="cat" breed="mixed" />
+      <SearchParams></SearchParams>
     </div>
   );
 };
--- src/searchParams.js
+++ src/searchParams.js
@@ -1,19 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  const location = "Seattle WA";
+  //all hook uses with "use", default state
+  //location = cur state, setLoc = updated state
+  //also this is destructuring
+  //hooks cannot go into for or if loop as the order are important,
+  //if one statement is not called then it would mess up the order of returned value
+
+  const [location, setLocation] = useState("Seattle WA");
+  //const [animal, setAnimal] = useState("dog");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  //const [breed, setBreed] = useState("");
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
-        <lable htmlFor="location">
+        <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location"></input>
-        </lable>
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            //when changed, onchange fired
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
+        </label>
+        <AnimalDropdown></AnimalDropdown>
+        <BreedDropdown></BreedDropdown>
         <button>Submit</button>
       </form>
     </div>
   );
 };
 
+/*
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <lable htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            //when changed, onchange fired
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
+        </lable>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={breed}
+            onchange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedStr) => (
+              <option key={breedStr} value={breedStr}>
+                {breedStr}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+*/
 export default SearchParams;
